7589953d7d4d942b92a4a72d2a9834bf2d1333e7
Cap <spluizguilherme@gmail.com>
2025-04-02 17:46:50 -0300

    ex07.py refatorado de forma mais simples e inteligente.

--- ex07.py
+++ ex07.py
@@ -11,112 +11,35 @@ n2 = int(input("Escreva o segundo número: "))
 n3 = int(input("Escreva o terceiro número: "))
 n4 = int(input("Escreva o quarto número: "))
 
+comando = str(input("[\"C\"] para crescente e [\"D\"] para decrescente: "))
+
 # Processamento
 
-# Maior
+if comando == "c" or comando == "C":
 
-if n1 >= n2 and n1 >= n3 and n1 >= n4:
-    maior = n1
+    while n1 > n2 or n2 > n3 or n3 > n4:
     
-elif  n2 >= n1 and n2 >= n3 and n2 >= n4:
-    maior = n2
-    
-elif n3 >= n2 and n3 >= n1 and n3 >= n4:
-    maior = n3
-    
-elif n4 >= n2 and n4 >= n3 and n4 >= n1:
-    maior = n4
-
-
-# Menor
-if n1 <= n2 and n1 <= n3 and n1 <= n4:
-    menor = n1
-    
-elif n2 <= n1 and n2 <= n3 and n2 <= n4:
-    menor = n2
-    
-elif n3 <= n2 and n3 <= n1 and n3 <= n4:
-    menor = n3
-    
-elif n4 <= n2 and n4 <= n3 and n4 <= n1:
-    menor = n4
-    
-
-# nums do Meio
-
-# N4
-if maior == n1 and menor == n4:
-    if n3 > n2:
-        print( menor,n2,n3,maior)
+        if n1 > n2:
+            n1 , n2 = n2, n1
         
-    else: print (menor ,n3,n2, maior)
+        elif n2 > n3:
+            n2 , n3 = n3, n2
     
-elif maior == n2 and menor == n4:
-    if n3 > n1:
-        print (menor,n1,n3,maior)
+        elif n3 > n4:
+            n3, n4 = n4, n3
+
+elif comando == "d" or comando == "D":
     
-    else: print (menor, n3, n1, maior)
-    
-elif maior == n3 and menor == n4:
-    if n1 > n2 :
-        print(menor, n2, n1, maior)
-    else: print(menor, n1, n2, maior)
-    
-    # inverte
-elif maior == n4 and menor == n1:
-    if n3 > n2:
-        print( menor,n2,n3,maior)
+    while n1 < n2 or n2 < n3 or n3 < n4:
         
-    else: print (menor ,n3,n2, maior)
-    
-elif maior == n4 and menor == n2:
-    if n3 > n1:
-        print (menor,n1,n3,maior)
-    
-    else: print (menor, n3, n1, maior)
-    
-elif maior == n4 and menor == n3:
-    if n1 > n2 :
-        print(menor, n2, n1, maior)
+        if n1 < n2:
+            n1 , n2 = n2, n1
         
-    else: print(menor, n1, n2, maior)
-
-# N3
-
-elif maior == n1 and menor == n3:
-    if n2 > n4:
-        print (menor, n4, n2, maior)
-    else: print (menor, n2, n4, maior)
+        elif n2 < n3:
+            n2 , n3 = n3, n2
     
-elif maior == n2 and menor == n3:
-    if n1 > n4:
-        print(menor, n4, n1, maior)
-    else: print(menor, n1, n4, maior)
-
-# Inverte
-
-elif maior == n3 and menor == n1:
-    if n2 > n4:
-        print (menor, n4, n2, maior)
+        elif n3 < n4:
+            n3, n4 = n4, n3
         
-    else:print(menor, n2, n4, maior )
-    
-elif maior == n3 and menor == n2:
-    if n1 > n4:
-        print (menor, n4, n1, maior)
-    else:print(menor, n1, n4, maior)
 
-# n2
-
-elif maior == n1 and menor == n2:
-    if n3 > n4:
-        print(menor, n4, n3, maior)
-    else: print(menor, n3, n4, maior)
-
-#inverte
-
-elif maior == n2 and menor == n1:
-    if n3 > n4:
-        print (menor, n4, n3, maior)
-    else:print(menor, n3, n4, maior)
-    
+print (n1,n2,n3,n4)    
